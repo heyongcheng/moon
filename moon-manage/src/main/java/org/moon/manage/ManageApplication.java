d3fc9df20bddac00be11731a74326a55beb47ba8
heyongcheng <178831731@qq.com>
2018-04-29 22:51:52 +0800

    init

---
+++ moon-manage/src/main/java/org/moon/manage/ManageApplication.java
@@ -0,0 +1,16 @@
+package org.moon.manage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by heyc on 2018/4/29.
+ */
+@SpringBootApplication
+public class ManageApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ManageApplication.class, args);
+    }
+
+}
